0879ce04c4f87e05fe11873d99108cd5a2585472
Hooda Akshat <hooda.akshat@syk.fi>
2022-08-28 12:53:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        boolean vacation=false;
+        boolean weekday=true;
+        if(weekday!=true||vacation==true)
+        {
+            System.out.println("James sleeps late today");
+        }
+    }
+}
